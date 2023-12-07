e5a7e6549ba7359a96677f7d383fe71c985fe6d8
damtran2005 <152955956+damtran2005@users.noreply.github.com>
2023-12-07 17:14:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = '';
+const giftUrl = '';
+const eventName = 'Chạm vào đuy 😉';
+const titleCard = 'happy birthday my baby🎇🎁';
+const contentCard = 'Thật buồn vì sn này k thể bên em😖.Anh đã cố hết sức tạo ra 1 món quà nhỏ này cho em chúc em sinh nhật zui zẻ luôn xinh đẹp , mọi thứ luôn thuận lợi và đặc biệt là mãi yêu anh nhó !Anh yêu em 😘❤️'
+const giftImage = 'anh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
